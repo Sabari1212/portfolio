3c2f2150cce0b07dd017b72d6112c59202ba9c70
Sabari1212 <146103181+Sabari1212@users.noreply.github.com>
2024-08-28 02:57:56 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,11 @@ const Contact = () => {
         <section className=' flex flex-col md:flex-row  px-5    bg-secondary ' id="contact">
            
            
-            <div className='  md:w-1/2 px-5 flex flex-col mr-15 md:justify-center pl-[100px]  '>
+            <div className='  md:w-1/2 px-5 flex flex-col mr-15 md:justify-center pl-[100px]  pr-10 '>
             <h className='font-bold text-white text-2xl md:text-4xl pt-[50px] border-b-4 border-black w-[120px] md:w-[160px]  '> CONTACT</h>
 
              <p className='font-bold text-white md:text-2xl pb-[20px] pt-5'>If you want to discuss more in detail.please contact me .</p>
-             <p className='font-bold text-white md:text-2xl pb-[20px]'><span className='font-bold contact-color'>EMAIL=</span>sabarisankar1212@</p>
+             <p className='font-bold text-white md:text-2xl pb-[20px]'><span className='font-bold contact-color'>EMAIL=</span>sabarisankar1212</p>
              <p className='font-bold text-white md:text-2xl pb-[20px]'><span className='font-bold contact-color'>PHONE=</span>9750371420</p>
 
             </div>
